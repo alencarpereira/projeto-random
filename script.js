79185047afb998b8e36f387f90a548442b992fe9
alencarpereira <suporte@alencarpereira.com>
2025-04-01 19:59:52 -0300

    adicionando if e else

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value);
     const max = Math.floor(document.querySelector(".input-max").value);
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    
-    alert(result);
+    if (min >= max) {
+        alert("O valor mínimo tem que ser menor que o valor máximo");
+        return;
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert("O número sorteado foi :  "  + result);
+    }
+
+
 }
